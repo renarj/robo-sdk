acae77297d41b9e3bda079d07b35958af4068357
Renze de Vries <renze@renarj.nl>
2016-03-16 23:37:45 +0100

    Oops corrected the range to be inverted

--- robo-core/src/main/java/com/oberasoftware/robo/core/sensors/AnalogToDistanceConverter.java
+++ robo-core/src/main/java/com/oberasoftware/robo/core/sensors/AnalogToDistanceConverter.java
@@ -11,7 +11,7 @@ public class AnalogToDistanceConverter implements SensorConverter<Double, Intege
     private static final Logger LOG = LoggerFactory.getLogger(AnalogToDistanceConverter.class);
 
     //TODO: Hardcoded for now, needs to be fixed
-    private final static Double MAX_SCALE_VOLT = 2.4;
+    private final static Double MAX_SCALE_VOLT = 2.33;
     private final static Double MIN_SCALE_VOLT = 0.02;
 
     private final static Double MAX_DISTANCE = 80.0;
@@ -24,7 +24,7 @@ public class AnalogToDistanceConverter implements SensorConverter<Double, Intege
         Double oldRange = MAX_SCALE_VOLT - MIN_SCALE_VOLT;
         Double newRange = MAX_DISTANCE - MIN_DISTANCE;
 
-        Double newValue = (((input - MIN_SCALE_VOLT) * newRange) / oldRange) + MIN_DISTANCE;
+        Double newValue = MAX_DISTANCE - (((input - MIN_SCALE_VOLT) * newRange) / oldRange);
 
         return newValue.intValue();
     }
