3f3953e5bf2ae8aad4df209b70d2b709bea9d7b4
Renze de Vries <renze@renarj.nl>
2016-05-16 21:43:18 +0200

    Letting converter use a message builder instead

--- robo-cloud/src/main/java/com/oberasoftware/robo/cloud/converters/DistanceEventConverter.java
+++ robo-cloud/src/main/java/com/oberasoftware/robo/cloud/converters/DistanceEventConverter.java
@@ -6,8 +6,7 @@ import com.oberasoftware.home.api.impl.types.ValueImpl;
 import com.oberasoftware.home.api.types.VALUE_TYPE;
 import com.oberasoftware.home.api.types.Value;
 import com.oberasoftware.home.core.mqtt.MQTTMessage;
-import com.oberasoftware.home.core.mqtt.MQTTMessageImpl;
-import com.oberasoftware.home.core.mqtt.MessageGroup;
+import com.oberasoftware.home.core.mqtt.MQTTMessageBuilder;
 import com.oberasoftware.robo.api.events.DistanceSensorEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,17 +19,19 @@ import org.springframework.stereotype.Component;
 public class DistanceEventConverter implements Converter {
     private static final Logger LOG = LoggerFactory.getLogger(DistanceEventConverter.class);
 
-    private static final String TOPIC_PATH = "/%s/%s/%s/%s";
-
     @TypeConverter
     public MQTTMessage convert(DistanceSensorEvent event) {
         LOG.info("Converting event: {} to mqtt", event);
 
-        String topic = String.format(TOPIC_PATH, MessageGroup.STATES.name(),
-                event.getRobotName(), event.getCapability(), event.getSourceName());
         Value value = new ValueImpl(VALUE_TYPE.NUMBER, event.getDistance());
 
-        LOG.info("Sending value: {} to topic: {}", value, topic);
-        return new MQTTMessageImpl(topic, value.asString());
+        MQTTMessage message = MQTTMessageBuilder.from(value)
+                .controller(event.getRobotName())
+                .channel(event.getCapability())
+                .label(event.getSourceName())
+            .build();
+
+        LOG.info("Sending value: {} MQTT message: {}", value, message);
+        return message;
     }
 }
