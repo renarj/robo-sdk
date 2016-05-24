cbe2b1e394f899469752728c2bfe0b0c5ed112e2
Renze de Vries <renze@renarj.nl>
2016-05-24 23:16:38 +0200

    Changed command handlers to work using BasicCommand principle

--- robo-cloud/src/main/java/com/oberasoftware/robo/cloud/handlers/RunMotionCommandHandler.java
+++ robo-cloud/src/main/java/com/oberasoftware/robo/cloud/handlers/RunMotionCommandHandler.java
@@ -1,7 +1,10 @@
 package com.oberasoftware.robo.cloud.handlers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.oberasoftware.base.event.EventHandler;
 import com.oberasoftware.base.event.EventSubscribe;
+import com.oberasoftware.home.api.commands.BasicCommand;
+import com.oberasoftware.home.api.model.BasicCommandImpl;
 import com.oberasoftware.home.core.mqtt.MQTTMessage;
 import com.oberasoftware.home.core.mqtt.MQTTPath;
 import com.oberasoftware.home.core.mqtt.MessageGroup;
@@ -11,12 +14,19 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 /**
  * @author Renze de Vries
  */
 @Component
 public class RunMotionCommandHandler implements EventHandler {
     private static final Logger LOG = LoggerFactory.getLogger(RunMotionCommandHandler.class);
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    static {
+        OBJECT_MAPPER.enableDefaultTyping();
+    }
 
     @Autowired
     private MotionEngine motionEngine;
@@ -25,8 +35,18 @@ public class RunMotionCommandHandler implements EventHandler {
     @MQTTPath(group = MessageGroup.COMMANDS, device = "motion", label = "run")
     public void convert(MQTTMessage mqttMessage) {
         LOG.info("Executing motion: {} from topic: {}", mqttMessage.getMessage(), mqttMessage.getTopic());
-        String motionCommand = mqttMessage.getMessage();
+        try {
+            BasicCommand basicCommand = OBJECT_MAPPER.readValue(mqttMessage.getMessage(), BasicCommandImpl.class);
 
-        motionEngine.runMotion(motionCommand);
+            String motionName = basicCommand.getProperties().get("motion");
+            if(motionName != null) {
+                LOG.info("Received motion execution: {}", motionName);
+                motionEngine.runMotion(motionName);
+            } else {
+                LOG.warn("Received motion command, but motion not specified");
+            }
+        } catch (IOException e) {
+            LOG.error("Could not load basic command", e);
+        }
     }
 }
--- robo-cloud/src/main/java/com/oberasoftware/robo/cloud/handlers/WalkMotionCommandHandler.java
+++ robo-cloud/src/main/java/com/oberasoftware/robo/cloud/handlers/WalkMotionCommandHandler.java
@@ -1,7 +1,10 @@
 package com.oberasoftware.robo.cloud.handlers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.oberasoftware.base.event.EventHandler;
 import com.oberasoftware.base.event.EventSubscribe;
+import com.oberasoftware.home.api.commands.BasicCommand;
+import com.oberasoftware.home.api.model.BasicCommandImpl;
 import com.oberasoftware.home.core.mqtt.MQTTMessage;
 import com.oberasoftware.home.core.mqtt.MQTTPath;
 import com.oberasoftware.home.core.mqtt.MessageGroup;
@@ -12,12 +15,19 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 /**
  * @author Renze de Vries
  */
 @Component
 public class WalkMotionCommandHandler implements EventHandler {
     private static final Logger LOG = LoggerFactory.getLogger(WalkMotionCommandHandler.class);
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    static {
+        OBJECT_MAPPER.enableDefaultTyping();
+    }
 
     @Autowired
     private MotionEngine motionEngine;
@@ -26,13 +36,26 @@ public class WalkMotionCommandHandler implements EventHandler {
     @MQTTPath(group = MessageGroup.COMMANDS, device = "motion", label = "walk")
     public void convert(MQTTMessage mqttMessage) {
         LOG.debug("Executing Walk: {} from topic: {}", mqttMessage.getMessage(), mqttMessage.getTopic());
-        WalkDirection walkDirection = WalkDirection.fromString(mqttMessage.getMessage());
-        LOG.info("Walking in direction: {}", walkDirection);
 
-        if(walkDirection != WalkDirection.STOP) {
-            motionEngine.walk(walkDirection);
-        } else {
-            motionEngine.stopWalking();
+        try {
+            BasicCommand basicCommand = OBJECT_MAPPER.readValue(mqttMessage.getMessage(), BasicCommandImpl.class);
+
+            String walkDirection = basicCommand.getProperties().get("direction");
+            if(walkDirection != null) {
+                WalkDirection direction = WalkDirection.fromString(mqttMessage.getMessage());
+                LOG.info("Walking in direction: {}", direction);
+
+                if(direction != WalkDirection.STOP) {
+                    motionEngine.walk(direction);
+                } else {
+                    motionEngine.stopWalking();
+                }
+            } else {
+                LOG.warn("Received walk command, but direction not specified");
+            }
+        } catch (IOException e) {
+            LOG.error("Could not load basic command", e);
         }
+
     }
 }
