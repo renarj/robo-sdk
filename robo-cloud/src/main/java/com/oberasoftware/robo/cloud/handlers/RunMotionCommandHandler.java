1eab45d127a3a4b82dbce83c1a948cfaf7fdbc9c
Renze de Vries <renze@renarj.nl>
2016-05-27 08:44:03 +0200

    Replaced ugly json object mapper code with utility method

--- robo-cloud/src/main/java/com/oberasoftware/robo/cloud/handlers/RunMotionCommandHandler.java
+++ robo-cloud/src/main/java/com/oberasoftware/robo/cloud/handlers/RunMotionCommandHandler.java
@@ -1,6 +1,5 @@
 package com.oberasoftware.robo.cloud.handlers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.oberasoftware.base.event.EventHandler;
 import com.oberasoftware.base.event.EventSubscribe;
 import com.oberasoftware.home.api.commands.BasicCommand;
@@ -14,7 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
+import static com.oberasoftware.home.util.ConverterHelper.mapFromJson;
 
 /**
  * @author Renze de Vries
@@ -23,11 +22,6 @@ import java.io.IOException;
 public class RunMotionCommandHandler implements EventHandler {
     private static final Logger LOG = LoggerFactory.getLogger(RunMotionCommandHandler.class);
 
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-    static {
-        OBJECT_MAPPER.enableDefaultTyping();
-    }
-
     @Autowired
     private MotionEngine motionEngine;
 
@@ -35,18 +29,14 @@ public class RunMotionCommandHandler implements EventHandler {
     @MQTTPath(group = MessageGroup.COMMANDS, device = "motion", label = "run")
     public void convert(MQTTMessage mqttMessage) {
         LOG.info("Executing motion: {} from topic: {}", mqttMessage.getMessage(), mqttMessage.getTopic());
-        try {
-            BasicCommand basicCommand = OBJECT_MAPPER.readValue(mqttMessage.getMessage(), BasicCommandImpl.class);
+        BasicCommand basicCommand = mapFromJson(mqttMessage.getMessage(), BasicCommandImpl.class);
 
-            String motionName = basicCommand.getProperties().get("motion");
-            if(motionName != null) {
-                LOG.info("Received motion execution: {}", motionName);
-                motionEngine.runMotion(motionName);
-            } else {
-                LOG.warn("Received motion command, but motion not specified");
-            }
-        } catch (IOException e) {
-            LOG.error("Could not load basic command", e);
+        String motionName = basicCommand.getProperties().get("motion");
+        if(motionName != null) {
+            LOG.info("Received motion execution: {}", motionName);
+            motionEngine.runMotion(motionName);
+        } else {
+            LOG.warn("Received motion command, but motion not specified");
         }
     }
 }
